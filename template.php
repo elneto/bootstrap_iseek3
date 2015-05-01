2e784f0237cbc93ec2ab5752450b1fef3a041433
lantyrn <hahnericm@gmail.com>
2015-05-01 16:01:05 -0400

    remove print_r for user login block

--- template.php
+++ template.php
@@ -45,7 +45,7 @@ function bootstrap_iseek3_form_alter( &$form, &$form_state, $form_id ) {
 
 function bootstrap_iseek3_form_user_login_block_alter(&$form, &$form_state, $form_id) {
 
-print_r($form);
+// print_r($form);
 
 	$form['name']['#description'] = ''; 
         $form['name']['#attributes']['placeholder'] = t('Enter your full name (e.g. Jane Doe)');
