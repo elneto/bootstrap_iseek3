c1821228ae1ac5f933ec15cf8ae010d803dcf522
Ernesto Araiza <araiza@un.org>
2015-05-05 16:27:43 -0400

    re added bottom region for footer

---
+++ templates/region--page-bottom.tpl.php
@@ -0,0 +1,175 @@
+<!-- Staff Toolkit -->
+      
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="toolkit large-text" id="toolkit-anchor"><i class="fa fa-briefcase fa-lg"></i> Toolkit</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-12 footer-border">
+        <div class="footer-background toolkit-row">
+        <div class="col-md-3 left-menu">
+           <div class="row underline left-menu-top">
+            <ul class="partners content-large-text">
+              <h5 class="content-large-text">Quicklinks - New York</h5>
+              <li><a href="">Emergency</a></li>
+              <li><a href="">Dag Hammarskjöld Library</a></li>
+              <li><a href="">Ask Dag (FAQ)</a></li>
+              <li><a href="">Journal </a></li>
+              <li><a href="">Learning UNHQ</a></li>
+              <li><a href="">OICT Services</a></li>
+              <li><a href="">Facilities iService Request (FMS)</a></li>
+              <li><a href="">Telephone Service Request (Unite)</a></li>
+              <li><a href="">Pass and ID </a></li>
+              <li><a href="">Wifi password</a></li>
+            </ul>
+          </div>
+          <div class="about-us">
+               <img src="images/iseek-logo-white.png" class="img-responsive" id="logo-footer" alt="iseek logo"/>
+                <p class="content-large-text"><a href="">About us <i class="fa fa-angle-double-right"></i></a></p>
+                <div class="medium-text"><p>The United Nations Intranet, iSeek, was developed in 2005 to encourage knowledge-sharing throughout the UN system. Its mission statement is: One intranet for one UN worldwide. </p></div>
+                <p class="content-large-text"><a href="">Contact us <i class="fa fa-angle-double-right"></i></a></p>
+        </div>
+        </div>
+          <div class="col-md-3">
+           <div class="footer-menu-items"> 
+            <h5 class="content-large-text underline">Key tools</h5>
+            <ul class="content-large-text" id="key-tools">
+              <li><a href="">Forms </a></li>
+              <li><a href="">Inspira </a></li>
+              <li><a href="">Umoja </a></li>
+              <li><a href="">Internal Webcasts </a></li>
+              <li><a href="">un.org </a></li>
+            </ul>
+            <ul class="medium-text">
+              <li><a href="">ARMS transfer eForm</a></li>
+              <li><a href="">Ask Dag</a></li>
+              <li><a href="">Business card generator</a></li>
+              <li><a href="">eMeets2.0</a></li>
+              <li><a href="">ePortfolio</a></li>
+              <li><a href="">eSubscription</a></li>
+              <li><a href="">Funds Monitoring Tool</a></li>
+              <li><a href="">gDoc</a></li>
+              <li><a href="">HR Portal</a></li>
+              <li><a href="">IMDIS</a></li>
+              <li><a href="">IMIS</a></li>
+              <li><a href="">iNeed</a></li>
+              <li><a href="">iTrack</a></li>
+              <li><a href="">ODS</a></li>
+              <li><a href="">Requisition Tracking</a></li>
+              <li><a href="">Travel Claim Portal</a></li>
+              <li><a href="">TRIP</a></li>
+              <li><a href="">UNBISnet</a></li>
+              <li><a href="">Unite Connections</a></li>
+              <li><a href="">Unite Docs</a></li>
+              <li><a href="">UNTERM</a></li>
+              <li><a href="">WIRe</a></li>          
+            </ul>
+            
+          </div>
+
+        </div>
+        <div class="col-md-3">
+          <div class="footer-menu-items"> 
+            <h5 class="content-large-text underline">Staff Development &amp; Learning</h5>
+            <ul class="medium-text">
+                <li><a href="">Career Portal</a></li>
+                <li><a href="">Examinations and tests</a></li>
+                <li><a href="">Language exams</a></li>
+                <li><a href="">Mandatory learning</a></li>
+                <li><a href="">YPP exam</a></li>
+            </ul>
+            <h5 class="content-large-text underline">Pay, benefits, Insurance</h5>
+            <ul class="medium-text">
+                <li><a href="">Benefits</a></li>
+                <li><a href="">Health Insurance</a></li>
+                <li><a href="">Local insurance offices</a></li>
+                <li><a href="">Salaries</a></li>
+                <li><a href="">Your Pay &amp; Benefits</a></li>
+            </ul>
+            <h5 class="content-large-text underline">Security</h5>
+            <ul class="medium-text">
+                <li><a href="">Emergency Management</a></li>
+                <li><a href="">Security and Safety Services around the world</a></li>
+            </ul>
+            <h5 class="content-large-text underline">Travel and Visas</h5>
+            <ul  class="medium-text">
+                <li><a href="">General Visa information</a></li>
+                <li><a href="">Official travel</a></li>
+                <li><a href="">Online travel booking</a></li>
+                <li><a href="">Travel policy</a></li>
+                <li><a href="">US and other Visas</a></li>
+            </ul>
+            <h5 class="content-large-text underline">Health and Wellbeing</h5>
+            <ul class="medium-text">
+                <li><a href="">Health Insurance</a></li>
+                <li><a href="">Medical alerts</a></li>
+                <li><a href="">Medical Services around the globe</a></li>
+                <li><a href="">Staff counsellor’s office</a></li>
+                <li><a href="">Travel medicine</a></li>
+                <li><a href="">UN Examining Physicians</a></li>
+            </ul>
+
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="footer-menu-items"> 
+            <h5 class="content-large-text underline">Rules and Regulations</h5>
+            <ul class="medium-text">
+                <li><a href="">Financial regulations</a></li>
+                <li><a href="">Flexible working arrangements</a></li>
+                <li><a href="">Generic Job Profiles</a></li>
+                <li><a href="">HR policies and Services</a></li>
+                <li><a href="">SGBs, AIs, ICs and more</a></li>
+                <li><a href="">The Charter of the United Nations</a></li>
+            </ul>
+            <h5 class="content-large-text underline">Reference and Manuals</h5>
+            <ul class="medium-text">
+                <li><a href="">Finance and Budget Manual</a></li>
+                <li><a href="">HR Manual</a></li>
+                <li><a href="">UN Correspondence Manual</a></li>
+                <li>UN Editorial Manual (<a href="">English</a> - <a href="">French</a>)</li>
+                <li><a href="">UN Journal</a></li>
+                <li><a href="">UN Libraries around the World</a></li>
+            </ul>
+
+            <h5 class="content-large-text underline">Ethics, Internal Justice</h5>
+            <ul class="medium-text">
+                <li><a href="">Accountability A to Z</a></li>
+                <li><a href="">Administration of Justice</a></li>
+                <li><a href="">Ethics Office</a></li>
+                <li><a href="">Ombudsman &amp; Mediation</a></li>
+                <li><a href="">Reporting Wrongdoing</a></li>
+            </ul>
+              <h5 class="content-large-text underline">Finance and budget</h5>
+            <ul class="medium-text">
+                <li><a href="">Financial regulations</a></li>
+                <li><a href="">Procurement</a></li>
+            </ul>
+          </div>
+        
+        
+        </div>
+              <div class="about-us-xs visible-xs visible-sm">
+                <img src="images/iseek-logo-white.png" class="img-responsive" id="logo-footer" alt="iseek logo"/>
+                <p class="content-large-text"><a href="">About us <i class="fa fa-angle-double-right"></i></a></p>
+                <div class="medium-text"><p>The United Nations Intranet, iSeek, was developed in 2005 to encourage knowledge-sharing throughout the UN system. Its mission statement is: One intranet for one UN worldwide. </p></div>
+                <p class="content-large-text"><a href="">Contact us <i class="fa fa-angle-double-right"></i></a></p>
+            </div>
+        </div>
+      </div>
+    </div>
+    </div><!-- / main .container -->
+    <p>&nbsp;</p>
+    <footer class="blog-footer visible-xs">
+      
+        <a href="#top-bar">Back to top</a>
+      
+     
+      <p>
+        All rights reserved. Copyright © United Nations 2015
+      </p>
+
+    </footer>
+  </div> <!-- main container -->
