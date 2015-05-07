e09fbc301f34b6f181c971df23c841acad47b2fa
Ernesto Araiza <araiza@un.org>
2015-05-07 17:23:05 -0400

    added page front template and static time zone/weather

---
+++ templates/page--front.tpl.php
@@ -0,0 +1,441 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a single Drupal page.
+ *
+ * The doctype, html, head and body tags are not in this template. Instead they
+ * can be found in the html.tpl.php template in this directory.
+ *
+ * Available variables:
+ *
+ * General utility variables:
+ * - $base_path: The base URL path of the Drupal installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $is_admin: TRUE if the user has permission to access administration pages.
+ *
+ * Site identity:
+ * - $front_page: The URL of the front page. Use this instead of $base_path,
+ *   when linking to the front page. This includes the language domain or
+ *   prefix.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $site_name: The name of the site, empty when display has been disabled
+ *   in theme settings.
+ * - $site_slogan: The slogan of the site, empty when display has been disabled
+ *   in theme settings.
+ *
+ * Navigation:
+ * - $main_menu (array): An array containing the Main menu links for the
+ *   site, if they have been configured.
+ * - $secondary_menu (array): An array containing the Secondary menu links for
+ *   the site, if they have been configured.
+ * - $breadcrumb: The breadcrumb trail for the current page.
+ *
+ * Page content (in order of occurrence in the default page.tpl.php):
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title: The page title, for use in the actual HTML content.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ * - $messages: HTML for status and error messages. Should be displayed
+ *   prominently.
+ * - $tabs (array): Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links (array): Actions local to the page, such as 'Add menu' on the
+ *   menu administration interface.
+ * - $feed_icons: A string of all feed icons for the current page.
+ * - $node: The node object, if there is an automatically-loaded node
+ *   associated with the page, and the node ID is the second argument
+ *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *   comment/reply/12345).
+ *
+ * Regions:
+ * - $page['help']: Dynamic help text, mostly for admin pages.
+ * - $page['highlighted']: Items for the highlighted content region.
+ * - $page['content']: The main content of the current page.
+ * - $page['sidebar_first']: Items for the first sidebar.
+ * - $page['sidebar_second']: Items for the second sidebar.
+ * - $page['header']: Items for the header region.
+ * - $page['footer']: Items for the footer region.
+ *
+ * @see bootstrap_preprocess_page()
+ * @see template_preprocess()
+ * @see template_preprocess_page()
+ * @see bootstrap_process_page()
+ * @see template_process()
+ * @see html.tpl.php
+ *
+ * @ingroup themeable
+ */
+?>
+
+
+<div id="top-bar">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3 col-md-6 col-sm-6 hidden-xs">
+				<!-- eric: base_url needs to be i18n and domain aware: is not currently -->
+				<img src="/sites/iseek.un.org/themes/bootstrap_iseek3/images/un-logo-top.png" border="0" id="un-top-logo" alt="United Nations logo"><a class="top-nav-item" href="<?php echo $GLOBALS['base_url']; ?>">Welcome to the United Nations Intranet</a>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-6">
+				<a class="top-nav-item" href="#">Low bandwidth | High contrast</a>
+			</div>
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<?php
+					$login_block = module_invoke('user', 'block_view', 'login');
+					print render($login_block['content']);
+                                        $user_menu_block = module_invoke('system', 'block_view', 'user-menu');
+                                        print render($user_menu_block['content']);
+				?>
+
+<!--
+				<span class="top-nav-item">Login</span>
+				<input type="text" id="login-name" name="login-name" placeholder="Name">
+				<input type="password" id="login-password" name="login-password" placeholder="Password">
+				<button name="login-go" class="btn" id="go-button">GO <i class="fa fa-angle-double-right"></i></button>
+-->
+			</div>
+			<div class="col-lg-2 col-md-6 col-sm-6">
+                                <?php
+                                        $block = module_invoke('locale', 'block_view', 'language');
+                                        print render($block['content']);
+                                ?>				
+				<?php // print render($page['header']); ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Main container -->
+<div class="container">
+
+	<div class="row">
+		<div class="col-md-3 col-xs-6">
+			<div class="row">
+				<div class="col-md-10 col-xs-12">
+					<a href="#">
+						<img src="<?php print $logo; ?>" onerror="this.onerror=null; this.src='images/iseek-logo.png'" border="0" class="img-responsive img-logo-banner" alt="iseek logo" width="225"/>
+					</a>
+				</div>
+				<div class="col-md-2 hidden-xs">
+					<div id="banner-separation"></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-7 col-xs-6">
+			<div class="navbar-default" id="location-navbar">
+				<ul class="nav navbar-nav">
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span id="location-display">New York</span> <span class="caret"></span></a>
+						<ul id="ul-location" class="dropdown-menu" role="menu">
+							<li><a href="#">Addis Ababa</a></li>
+							<li><a href="#">Bangkok</a></li>
+							<li><a href="#">Beirut</a></li>
+							<li><a href="#">Geneva</a></li>
+							<li><a href="#">Nairobi</a></li>
+							<li><a href="#">New York</a></li>
+							<li><a href="#">Santiago</a></li>
+							<li><a href="#">Vienna</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+		<div class="col-md-2 visible-md visible-lg">
+			<img src="/sites/iseek.un.org/themes/bootstrap_iseek3/images/un-70.svg" onerror="this.onerror=null; this.src='/sites/iseek.un.org/themes/bootstrap_iseek3/images/un-70.png'" border="0" class="img-responsive img-logo-banner" alt="UN 70 logo" width="145">
+		</div>
+	</div>
+
+
+
+
+
+
+</div>
+<!-- /Main container -->
+
+
+<div class="main-container container">
+
+  <div class="row">
+
+	<div class="col-sm-10">
+
+		<?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+		  <div class="navbar-collapse collapse">
+			<nav role="navigation">
+			  <?php if (!empty($primary_nav)): ?>
+				<?php print render($primary_nav); ?>
+			  <?php endif; ?>
+			  <?php if (!empty($secondary_nav)): ?>
+				<?php print render($secondary_nav); ?>
+			  <?php endif; ?>
+			  <?php if (!empty($page['navigation'])): ?>
+				<?php print render($page['navigation']); ?>
+			  <?php endif; ?>
+			</nav>
+		  </div>
+		<?php endif; ?>
+
+	</div>
+
+	
+	<div class="col-sm-2 col-xs-12">
+		<?php print render($page['header']); ?>
+	</div>
+
+  </div>
+
+
+  <div class="row">
+
+	<div class="container">
+		<div class="row">
+
+		
+				
+
+			<!-- center1 -->
+		
+			<div class="col-xs-12 col-sm-6 col-sm-push-3">
+				<section<?php //print $content_column_class; ?>>
+
+
+				  <?php if (!empty($page['highlighted'])): ?>
+					<div class="highlighted"><?php print render($page['highlighted']); ?></div>
+				  <?php endif; ?>
+				  <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+				  <a id="main-content"></a>
+				  <?php print render($title_prefix); ?>
+				  <?php if (!empty($title)): ?>
+					<h1 class="page-header"><?php print $title; ?></h1>
+				  <?php endif; ?>
+				  <?php print render($title_suffix); ?>
+				  <?php print $messages; ?>
+				  <?php if (!empty($tabs)): ?>
+					<?php print render($tabs); ?>
+				  <?php endif; ?>
+				  <?php if (!empty($page['help'])): ?>
+					<?php print render($page['help']); ?>
+				  <?php endif; ?>
+				  <?php if (!empty($action_links)): ?>
+					<ul class="action-links"><?php print render($action_links); ?></ul>
+				  <?php endif; ?>
+				  <?php print render($page['content']); ?>
+
+				</section>
+			</div>
+
+				<!-- /center1 -->
+
+				<!-- right -->
+
+
+					<div class="col-xs-12 col-sm-3 col-sm-push-3">
+						<?php if (!empty($page['sidebar_second'])): ?>
+						  <!-- <aside class="col-sm-3" role="complementary"> -->
+						  <aside role="complementary">
+							<?php print render($page['sidebar_second']); ?>
+						  </aside>  <!-- /#sidebar-second -->
+						<?php endif; ?>
+					</div>
+
+
+				<!-- /right -->
+				
+				<!-- left -->
+
+
+					<div class="col-xs-12 col-sm-3 col-sm-pull-9">
+
+						<?php if (!empty($page['sidebar_first'])): ?>
+						  <!-- <aside class="col-sm-3" role="complementary"> -->
+						  <aside role="complementary">
+							<?php print render($page['sidebar_first']); ?>
+						  </aside>  <!-- /#sidebar-first -->
+						<?php endif; ?>
+
+					</div>
+
+				<!-- /left -->
+		</div> <!-- row -->
+	</div> <!-- container -->
+  </div> <!-- row -->
+
+<!-- Time zone and weather -->
+   <div class="row">
+        <div class="col-lg-12">
+        <div class="timezone">&nbsp;&nbsp;&nbsp;<i class="fa fa-sun-o fa-2x"></i>&nbsp;&nbsp;&nbsp;<span class="large-text">Time zone and weather</span></div></div>
+      </div>
+
+      <div class="row">
+          <div class="col-md-12 col-lg-6 timezone-pad-r0">
+            <div class="dutystaion-container ">
+            
+              <div class="col-md-3 col-xs-6 dutystaion">
+
+                
+                  <div class="large-text duty-margin">Addis Ababa</div>
+                  <div class=" medium-text duty-margin">27 February</div>
+                  <div class=" medium-text duty-margin time">21:37</div>
+                
+                <div class="row duty-padding temp">
+                  <div class="col-lg-3 weather-icon moon">      
+                    <i class="fa fa-moon-o  fa-3x"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        32 &#8451; / 83  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding ">
+                      32 &#8451; / 83  &#8457;
+                    </div>
+                    
+                  </div>
+                </div>
+
+              </div>
+              <div class="col-md-3 col-xs-6 dutystaion">
+                
+                <div class="row large-text duty-margin">Bangkok</div>
+                <div class="row medium-text duty-margin">28 February</div>
+                <div class="row medium-text duty-margin time">01:37</div>
+                <div class="row duty-padding">
+                  <div class="col-lg-3 weather-icon moon">      
+                    <i class="fa fa-moon-o  fa-3x"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        30&#8451; / 80  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding temp">
+                      23&#8451; / 83  &#8457;
+                </div>
+
+              </div>
+             </div>
+           </div>
+              <div class="col-md-3 col-xs-6 dutystaion">                
+                <div class="row large-text duty-margin">Beirut</div>
+                <div class="row medium-text duty-margin">27 February</div>
+                <div class="row medium-text duty-margin time">20:37</div>
+                <div class="row duty-padding">
+                  <div class="col-lg-3 weather-icon moon">      
+                    <i class="fa fa-moon-o  fa-3x"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        28&#8451; / 83  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding temp">
+                      32&#8451; / 83  &#8457;
+                    </div>
+</div>
+</div>
+           </div>
+             
+              <div class="col-md-3 col-xs-6 dutystaion">                
+                <div class="row duty-margin large-text">Geneva</div>
+                <div class="row medium-text duty-margin">27 February</div>
+                <div class="row medium-text duty-margin time">19:37</div>
+                
+                <div class="row duty-padding">
+                  <div class="col-lg-3 weather-icon sun">      
+                    <i class="fa fa-sun-o fa-3x sun"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        32&#8451; / 83  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding temp">
+                      32&#8451; / 83  &#8457;
+                    </div>
+</div>
+</div>
+           </div>
+            
+          </div></div>
+          <div class="col-md-12 col-lg-6 timezone-pad-l0">
+          <div class="dutystaion-container ">
+              <div class="col-md-3 col-xs-6 dutystaion">                
+                <div class="row large-text duty-margin">Nairobi</div>
+                <div class="row medium-text duty-margin">27 February</div>
+                <div class="row medium-text duty-margin time">18:37</div>
+                <div class="row duty-padding">
+                  <div class="col-lg-3 weather-icon moon">      
+                    <i class="fa fa-moon-o  fa-3x"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        32&#8451; / 83  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding temp">
+                      32&#8451; / 83  &#8457;
+                    </div>
+</div>
+</div>
+           </div>
+              <div class="col-md-3 col-xs-6 dutystaion">
+                <div class="row large-text duty-margin">New York</div>
+                <div class="row medium-text duty-margin">27 February</div>
+                <div class="row medium-text duty-margin time">13:37</div>
+                <div class="row duty-padding">
+                  <div class="col-lg-3 weather-icon rain">      
+                    <i class="fa fa-tint fa-3x"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        32&#8451; / 83  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding temp">
+                      32&#8451; / 83  &#8457;
+                    </div>
+</div>
+</div>
+                
+           </div>
+              <div class="col-md-3 col-xs-6 dutystaion">                
+                <div class="row large-text duty-margin">Santiago</div>
+                <div class="row medium-text duty-margin">27 February</div>
+                <div class="row medium-text duty-margin time">15:37</div>
+                <div class="row duty-padding">
+                  <div class="col-lg-3 weather-icon sun">      
+                    <i class="fa fa-sun-o fa-3x"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        32&#8451; / 83  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding temp">
+                      32&#8451; / 83  &#8457;
+                    </div>
+</div>
+</div>
+           </div>
+              <div class="col-md-3 col-xs-6 dutystaion-last">                
+                <div class="row large-text duty-margin">Vienna</div>
+                <div class="row medium-text duty-margin">27 February</div>
+                <div class="row medium-text duty-margin time">21:37</div>
+                <div class="row duty-padding">
+                  <div class="col-lg-3 weather-icon cloud">      
+                    <i class="fa fa-cloud fa-3x"></i>
+                  </div>
+                  <div class="col-lg-9">      
+                    <div class="row medium-text duty-padding">
+                        32&#8451; / 83  &#8457;
+                    </div>
+                    <div class="row medium-text duty-padding temp">
+                      32&#8451; / 83  &#8457;
+                    </div>
+</div>
+            
+          </div>
+      </div>
+      </div></div>
+           </div>
+<!-- Ends: Time zone and weather -->
