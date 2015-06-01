74c281d14b19e84f6c61bd7cc700d496dec71400
Ernesto Araiza <araiza@un.org>
2015-06-01 12:19:42 -0400

    articles still not working

--- templates/views-view-unformatted--articles--page.tpl.php
+++ templates/views-view-unformatted--articles--page.tpl.php
@@ -6,14 +6,21 @@
  *
  * @ingroup views_templates
  */
+//kpr($rows);
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<?php foreach ($rows as $id => $row): ?>
+<?php 
+//kpr(get_defined_vars());
+//kpr($view->field->field_location);
+foreach ($rows as $id => $row): ?>
 <div class="row">
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>> <!-- internal div -->
-    <?php print $row; ?>
+    <?php print $row; 
+    //kpr($row);
+    //kpr($array);
+    ?>
   	<!-- </div> --> <!-- The end of the col-md-2 started in views view-field-articles-page-comment template -->  
   </div> <!-- end of internal div -->
 </div> <!-- end of row -->
