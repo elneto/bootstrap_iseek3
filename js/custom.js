f81f79c057690bc2be337e5123eebf2f8f60e348
lantyrn <hahnericm@gmail.com>
2015-06-19 17:38:31 -0400

    add 2000ms to toolkit action

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,9 @@ jQuery(document).ready(function () {
 	jQuery("#block-menu-menu-iseek3-main-menu > ul.nav > li.leaf:nth-child(6) > a").click(function (e) {
 		e.preventDefault();
                 e.stopPropagation();
-		jQuery('#toolkit-anchor').ScrollTo();
+		jQuery('#toolkit-anchor').ScrollTo({
+    			duration: 2000,
+    			easing: 'linear'
+		});
 	});
 });
