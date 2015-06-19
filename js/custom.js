ad2ba913ed65d95fdbcd74ce67ee9ce9bd9bedf4
lantyrn <hahnericm@gmail.com>
2015-06-19 17:07:08 -0400

    adding action to toolkit link for home page

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function () {
+	jQuery("#block-menu-menu-iseek3-main-menu > ul.nav > li.leaf:nth-child(6) > a").click(function (e) {
+		e.preventDefault();
+                e.stopPropagation();
+		jQuery('#toolkit-anchor').ScrollTo();
+	});
+});
