656d2451fe1bfa10ba56e17cb72d3fe45235605e
KSD <KSD@husainsalah.users.unhq.un.org>
2015-07-02 11:03:21 -0400

    Basic page template to accomaodate title

--- templates/node--page.tpl.php
+++ templates/node--page.tpl.php
@@ -86,7 +86,7 @@
 
   <?php print render($title_prefix); ?>
   <?php // if (!$page): ?>
-    <div class="toolkit large-text"><h2><?php print $title_attributes; ?><?php print $title; ?></h2></div>
+    <div class="toolkit large-text"><?php print $title_attributes; ?><?php print $title; ?></div>
   <?php // endif; ?>
   <?php print render($title_suffix); ?>
 
