3660452c32d8de58e08a8ca530f88e1d72c04bf6
lantyrn <hahnericm@gmail.com>
2015-07-20 17:00:28 -0400

    adding user login template 4

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -9,43 +9,34 @@
 
 <div class="row">
 	<div class="col-lg-12">
-		<div class="top-side-box"> <?php echo t('Welcome to the United Nations Intranet');?></div>
-	
-		<p>
-			<?php echo t('The United Nations Intranet, iSeek, was developed to encourage knowledge-sharing throughout the UN system.');?>
-			<br/>
-			<?php echo t('Log in now using your Lotus Notes webmail account and be a part of the discussion!');?> 
-		</p>
+		<div class="toolkit large-text" id="toolkit-anchor"> <?php echo t('Welcome to the United Nations Intranet');?></div>
 	</div>
 </div>
 
+<div class="row">
+        <div class="col-lg-12">
+		<p id="user_login_intro">
+                        <?php echo t('The United Nations Intranet, iSeek, was developed to encourage knowledge-sharing throughout the UN system.');?>
+                        <br/>
+                        <?php echo t('Log in now using your Lotus Notes webmail account and be a part of the discussion!');?>
+                </p>
+        </div>
+</div>
 
-
-<h3>name</h3>
-
-<?php print drupal_render($form['name']); ?>
-
-<h3>pass</h3>
-
-<?php    print drupal_render($form['pass']); ?>
-
-<h3>form_build_id</h3>
-
-<?php    print drupal_render($form['form_build_id']); ?>
-
-<h3>form_id</h3>
-
-<?php    print drupal_render($form['form_id']); ?>
-
-<h3>actions</h3>
-
-<?php    print drupal_render($form['actions']); ?>
-
+<div class="row">
+        <div class="col-lg-12" id="user_login_form">
+		<?php print drupal_render($form['name']); ?>
+		<?php print drupal_render($form['pass']); ?>
+		<?php print drupal_render($form['form_build_id']); ?>
+		<?php print drupal_render($form['form_id']); ?>
+		<?php print drupal_render($form['actions']); ?>
+	</div>
+</div>
 
 <div class="row">
         <div class="col-lg-12">
-		<p>
-			<?php echo t('If you have difficulties accessing the system with your Lotus Notes webmail account or if you use Exchange or Outlook, please contact the UN Intranet Team.');?>
+		<p id="user_login_intro">
+			<?php echo t('* If you have difficulties accessing the system with your Lotus Notes webmail account or if you use Exchange or Outlook, please contact the UN Intranet Team.');?>
 		</p>
         </div>
 </div>
