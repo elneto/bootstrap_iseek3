9c30c9e070e731bcafe3f603c209ba6b7d999cae
lantyrn <hahnericm@gmail.com>
2015-07-20 16:42:05 -0400

    adding user login template 2

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -4,4 +4,26 @@
   print '<pre>';
   print_r($variables);
   print '</pre>';
+
 ?>
+
+<h3>name</h3>
+
+<?php print drupal_render($form['name']); ?>
+
+<h3>pass</h3>
+
+<?php    print drupal_render($form['pass']); ?>
+
+<h3>form_build_id</h3>
+
+<?php    print drupal_render($form['form_build_id']); ?>
+
+<h3>form_id</h3>
+
+<?php    print drupal_render($form['form_id']); ?>
+
+<h3>actions</h3>
+
+<?php    print drupal_render($form['actions']); ?>
+
