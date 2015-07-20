03e0677f7b251fda706c38805a437ffd968b5aff
lantyrn <hahnericm@gmail.com>
2015-07-20 16:54:55 -0400

    adding user login template 3

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -1,11 +1,25 @@
 <?php
   /* print the variables if needed to allow for 
   individual field theming or breaking them up. */
-  print '<pre>';
-  print_r($variables);
-  print '</pre>';
+//  print '<pre>';
+//  print_r($variables);
+//  print '</pre>';
 
 ?>
+
+<div class="row">
+	<div class="col-lg-12">
+		<div class="top-side-box"> <?php echo t('Welcome to the United Nations Intranet');?></div>
+	
+		<p>
+			<?php echo t('The United Nations Intranet, iSeek, was developed to encourage knowledge-sharing throughout the UN system.');?>
+			<br/>
+			<?php echo t('Log in now using your Lotus Notes webmail account and be a part of the discussion!');?> 
+		</p>
+	</div>
+</div>
+
+
 
 <h3>name</h3>
 
@@ -27,3 +41,11 @@
 
 <?php    print drupal_render($form['actions']); ?>
 
+
+<div class="row">
+        <div class="col-lg-12">
+		<p>
+			<?php echo t('If you have difficulties accessing the system with your Lotus Notes webmail account or if you use Exchange or Outlook, please contact the UN Intranet Team.');?>
+		</p>
+        </div>
+</div>
