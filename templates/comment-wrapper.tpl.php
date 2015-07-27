43141cda0d0db8f41ffa4123218287a719e094cd
KSD <KSD@husainsalah.users.unhq.un.org>
2015-07-27 18:15:48 -0400

    comment wrapper for print

--- templates/comment-wrapper.tpl.php
+++ templates/comment-wrapper.tpl.php
@@ -47,7 +47,7 @@
 
   <?php print render($content['comments']); ?>
 
-
+<div class="hidden-print">
   <?php if ($content['comment_form']): ?>
     <h2 class="title comment-form"><?php print t('Add new comment'); ?></h2>
 	<?php
@@ -61,4 +61,5 @@
     <?php print render($content['comment_form']); ?>
 
   <?php endif; ?>
+</div>
 </div>
